864812fa8fd1b181add8bc87fbb8ed5dc39f20d2
mamun-63 <mamun.cse63@gmail.com>
2022-05-15 23:57:59 +0600

    chore: basic setup and type understand

---
+++ output/script.js
@@ -0,0 +1,19 @@
+"use strict";
+// const multiply = (a: number, b: number) => {
+//   return a * b
+// }
+// console.log(multiply(7, 4))
+// ___ array 
+let fruits = ['Orange', 'Banana', 'Mango'];
+fruits.push('Pineapple');
+console.log(fruits);
+let mixed = ['Mamun', 26, true];
+mixed.push('New');
+// ___ object
+let person = {
+    name: 'Mamun',
+    age: 26,
+    isDeveloper: true
+};
+// Can't declare instead of the above scheme/ structure
+// person.country = 'Bangladesh'
