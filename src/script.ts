ed1c9c93ffbcdce288ef979c95e09eba408fd711
mamun-63 <mamun.cse63@gmail.com>
2022-06-14 12:07:46 +0600

    interface

--- src/script.ts
+++ src/script.ts
@@ -4,91 +4,88 @@
 
 // console.log(multiply(7, 4))
 
+// ___ array
+let fruits = ["Orange", "Banana", "Mango"];
+fruits.push("Pineapple");
+console.log(fruits);
 
-// ___ array 
-let fruits = ['Orange', 'Banana', 'Mango']
-fruits.push('Pineapple')
-console.log(fruits)
-
-let mixed = ['Mamun', 26, true]
-mixed.push('New')
+let mixed = ["Mamun", 26, true];
+mixed.push("New");
 
 // ___ object
 let person = {
-  name: 'Mamun',
+  name: "Mamun",
   age: 26,
-  isDeveloper: true
-}
+  isDeveloper: true,
+};
 // Can't declare instead of the above scheme/ structure
 // person.country = 'Bangladesh'
-
 
 /**  Explicit and Union Types*/
 
 // normal variable
 let a: number | string;
-a = 'Mamun'
-a = 26
+a = "Mamun";
+a = 26;
 
 // array
 let b: (number | string)[] = [];
-b.push('Mamun', 26)
+b.push("Mamun", 26);
 
 // object
 let c: object;
 c = {
-  name: 'Mamun',
-  age: 26
-}
+  name: "Mamun",
+  age: 26,
+};
 
 let obj: {
-  name: string,
-  age: number,
-  adult: boolean
-}
+  name: string;
+  age: number;
+  adult: boolean;
+};
 obj = {
-  name: 'Mamun',
+  name: "Mamun",
   age: 26,
-  adult: true
-}
+  adult: true,
+};
 
 let d: object;
 // it accepts, because array is also a certain type of object in js
-d = [1,2,3,4]
+d = [1, 2, 3, 4];
 
 // any
 let e: any;
-let arr: any[] = []
-arr.push('Mamun', 25)
+let arr: any[] = [];
+arr.push("Mamun", 25);
 let ob: any;
 ob = {
-  name: 'Mamun',
+  name: "Mamun",
   age: 26,
-  adult: true
-}
+  adult: true,
+};
 
 /**  How to use Functions  */
 let myFunc: Function;
 myFunc = () => {
-  console.log('Hello! I am function.')
-}
+  console.log("Hello! I am function.");
+};
 
 // c?: string  - optional
-let yourFunc = (a: string, b: string, c?: string, d: string = 'BR') => {
-  console.log(`Hey ${a} ${b} ${c} ${d}`)
+let yourFunc = (a: string, b: string, c?: string, d: string = "BR") => {
+  console.log(`Hey ${a} ${b} ${c} ${d}`);
 
   // ts returns void default
   // js - returns undefined
   // void means nothing; undefined is value, but nothing defined
   return a + b;
-}
-yourFunc('Mamun', 'Abdullah')
+};
+yourFunc("Mamun", "Abdullah");
 
 let numb = (a: number, b: number): number => {
   return a + b;
-}
-console.log(numb(2,4))
-
+};
+console.log(numb(2, 4));
 
 /**  Type Aliases */
 
@@ -100,26 +97,24 @@ console.log(numb(2,4))
 // }
 
 type stringOrNum = string | number;
-type userType = { name: string, age: number }
+type userType = { name: string; age: number };
 
 const userDetails = (id: stringOrNum, user: userType) => {
-  console.log(`User name is ${user.name}, id: ${id}, age: ${user.age}`)
-}
+  console.log(`User name is ${user.name}, id: ${id}, age: ${user.age}`);
+};
 
 const sayHello = (user: userType) => {
-  console.log(`Hello ${user.age > 30 ? 'Sir' : 'Mr.'} ${user.name}`)
-}
-
+  console.log(`Hello ${user.age > 30 ? "Sir" : "Mr."} ${user.name}`);
+};
 
 /** Functions signature */
-let calculations: (x: number, y: number, z: string) => number
+let calculations: (x: number, y: number, z: string) => number;
 calculations = (a: number, b: number, c: string) => {
-  if (c === 'add') return a + b;
+  if (c === "add") return a + b;
   else return a - b;
-}
+};
 
-console.log(calculations(5,16,'add'))
-
+console.log(calculations(5, 16, "add"));
 
 /** Class  */
 
@@ -134,9 +129,9 @@ console.log(calculations(5,16,'add'))
 //   //   this.country = c;
 //   // }
 
-//   // if define access modifier then, can minimize 
+//   // if define access modifier then, can minimize
 //   constructor(
-//     private name: string, 
+//     private name: string,
 //     public age: number,
 //     readonly country: string
 //     ) {}
@@ -146,24 +141,49 @@ console.log(calculations(5,16,'add'))
 //   }
 // }
 
-import { Player } from "./classes/Player.js"
+import { Player } from "./classes/Player.js";
 
-const mamun = new Player('Abdullah Al Mamun', 25, 'Bangladesh')
-const nuhas = new Player('Anjum Nuhas', 27, 'Bangladesh')
+const mamun = new Player("Abdullah Al Mamun", 25, "Bangladesh");
+const nuhas = new Player("Anjum Nuhas", 27, "Bangladesh");
 
 // mamun.name = 'Rothi'
 mamun.age = 24;
 // mamun.country = 'Pakistan';
 // console.log(mamun.name)
-console.log(mamun.age)
+console.log(mamun.age);
 
 // private mode: no modification and access
 // readonly: can access but cant modify
 
-const players: Player[] = []
+const players: Player[] = [];
 players.push(mamun);
 players.push(nuhas);
 
-console.log(players)
+console.log(players);
 
+/** Interface */
 
+// interface in object
+interface RectangleOptions {
+  width: number;
+  length: number;
+}
+
+const drawRectangle = (options: RectangleOptions) => {
+  let width = options.width;
+  let length = options.length;
+};
+
+drawRectangle({
+  width: 30,
+  length: 20,
+  // height: 28 // Can't put this
+});
+
+let threeDptions = {
+  width: 30,
+  length: 20,
+  height: 15,
+};
+
+drawRectangle(threeDptions);
